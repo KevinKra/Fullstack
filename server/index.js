f853b6315c8a4157f89694601cdf4fd2238c75b2
Kevin Krato <kratok08@gmail.com>
2019-08-08 15:55:27 -0600

    Move routes and services out into seperate dirs

--- server/index.js
+++ server/index.js
@@ -1,35 +1,11 @@
 const express = require("express");
 const Joi = require("@hapi/joi");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
+
 const app = express();
+require("./routes/authRoutes")(app);
 
 app.use(express.json());
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
 
 // const books = [
 //   { id: 1, name: "The Journey Not Traveled", author: "Scott Moore" },
